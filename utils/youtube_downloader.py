bc69b033558d2c9cc0d8514065a52fa117f0941a
audrye <augustin.drye@gmail.com>
2025-05-13 16:56:17 +0200

    DL in youtube

--- utils/youtube_downloader.py
+++ utils/youtube_downloader.py
@@ -1,7 +1,23 @@
-from pytube import YouTube
+# from pytube import YouTube
+
+# def download_video(url, output_path="outputs/video.mp4"):
+#     yt = YouTube(url)
+#     stream = yt.streams.filter(file_extension='mp4', progressive=True).get_highest_resolution()
+#     stream.download(filename=output_path)
+#     return output_path
+
+import yt_dlp
 
 def download_video(url, output_path="outputs/video.mp4"):
-    yt = YouTube(url)
-    stream = yt.streams.filter(file_extension='mp4', progressive=True).get_highest_resolution()
-    stream.download(filename=output_path)
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]',
+        'outtmpl': output_path,
+        'quiet': True,
+        'noplaylist': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
     return output_path
+
