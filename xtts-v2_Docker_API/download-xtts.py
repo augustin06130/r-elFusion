70577972734fb0cae2f598117861bd68a7d83859
Nicolazic_Tardy <56249852+Bidiche49@users.noreply.github.com>
2025-06-05 19:56:38 +0200

    Update download-xtts.py

--- xtts-v2_Docker_API/download-xtts.py
+++ xtts-v2_Docker_API/download-xtts.py
@@ -6,7 +6,7 @@ model_dir = "xtts-v2-model"
 if not os.path.isdir(model_dir):
 	print("Téléchargement du modèle XTTS depuis Hugging Face...")
 	snapshot_download(
-		repo_id="Bidiche/xtts-v2-model",
+		repo_id="coqui/XTTS-v2",
 		repo_type="model",
 		local_dir=model_dir,
 		local_dir_use_symlinks=False
